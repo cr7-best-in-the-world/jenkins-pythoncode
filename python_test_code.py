692d0a871946b6716b4f9ea57eabdf0b8a2b37ee
HARSH KUMAR <56390599+cr7-best-in-the-world@users.noreply.github.com>
2024-08-08 10:48:02 +0530

    Create python_test_code.py
    adding a simple python code to display area of Rectangle and Circle using Polymorphism concept

---
+++ python_test_code.py
@@ -0,0 +1,29 @@
+from math import pi
+
+
+class Shape:
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.height * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * (self.radius ** 2)
+
+
+shapes = [Rectangle(4, 5), Circle(3), Rectangle(6, 7), Circle(4)]
+
+for shape in shapes:
+    print(f"area of the shape is: {shape.area()}")
