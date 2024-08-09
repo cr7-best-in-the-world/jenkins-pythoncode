4377b10b9426691d122dfb959f5ebcbe2c5a6d40
HARSH KUMAR <56390599+cr7-best-in-the-world@users.noreply.github.com>
2024-08-09 12:18:54 +0530

    Update test.py
    checking failed build

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 print("Wassup Buddy!! v11 - checking git polling ")
 print("Wassup Buddy!! v12 - checking git polling ")
 print("Wassup Buddy!! v12 - checking git polling ")
+
+print("checking failed build pipeline through email notification "
